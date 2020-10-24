b99ba841a1b2a27fd0fdc463f34e22ca22839f77
Mattias Ahlsen <mattias.ahlsen@gmail.com>
2020-10-24 16:39:38 +0200

    Added BaseSelect

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <!--<HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>-->
+
+  <BaseSelect
+    label="Region"
+    :options="['Stockholm', 'Hela Sverige']"
+    v-model="region"
+  />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import BaseSelect from './components/BaseSelect.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    BaseSelect,
+  },
+  data() {
+    return {
+      region: null,
+    }
   }
 });
 </script>
@@ -23,5 +34,10 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.field {
+  border: 1px solid #222;
+  font-size: 1.2rem;
 }
 </style>
---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,52 @@
+<template>
+  <label v-if="label">{{label}}</label>
+  <select
+    :value="modelValue"
+    class="field"
+    v-bind="{
+      ...$attrs,
+      onChange: $event => { $emit('update:modelValue', $event.target.value) }
+    }"
+  >
+    <option
+      v-for="option in options"
+      :key="option"
+      :value="option"
+      :selected="option === modelValue"
+    >{{ option }}</option>
+  </select>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+  }  
+})
+</script>
+
+<style lang="scss" scoped>
+select {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  cursor: pointer;
+}
+
+select::-ms-expand {
+  display: none;
+}
+</style>
